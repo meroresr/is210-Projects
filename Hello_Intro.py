dc180a1cacf09bbf59e9218f70732e7f783c18e7
Reginald <reginald.merores14@spsmail.cuny.edu>
2019-09-04 17:11:43 -0400

    I added this python file

---
+++ Hello_Intro.py
@@ -0,0 +1,4 @@
+yourName = input("What is your name? ")
+yourLocation = input("Where do you live? ")
+
+print("Hello " + yourName + "." + " How far do you take to go home for " + yourLocation + "?")
